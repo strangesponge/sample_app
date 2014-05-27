cd74055c30406edd7864cd7936bff75f8f104824
Peter Grogan <strangesponge@gmail.com>
2014-05-27 12:18:11 +0100

    nearly done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,26 @@
 class ProductsController < ApplicationController
+
 before_filter :authorize, only: [:edit, :update]
+
   def show
-    @product = Product.find(:all)
+    @cleaner = Product.find(1)
+    @spray = Product.find(2)
+    @wipes = Product.find(3)
   end
 
   def floorcleaner
-    @product = Product.find(:all)
+    @product = Product.find(1)
+      @stock = @product.stock
   end
 
   def kitchenspray
-    @product = Product.find(:all)
+    @product = Product.find(2)
+      @stock = @product.stock
   end	
 
   def wipes
-    @product = Product.find(:all)
+    @product = Product.find(3)
+      @stock = @product.stock
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to catalog_path, notice: "Logged in!"
+      redirect_to catalogue_path
     else
       flash.now.alert = "Email or password is invalid"
       render "new"
     end
   end
   
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out!"
-  end
 end
